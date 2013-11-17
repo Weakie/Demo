6f4236166f8e1a031b483578ec67d54e36b09a83
weakie <weakielin@gmail.com>
2013-11-17 15:38:29 +0800

    change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 import java.io.*;
 public class HelloWorld{
 	public static void main(String[] args){
+		String name = args[0];
+		System.out.println(name);
 		System.out.println("Hello World!");
 	}
 }
